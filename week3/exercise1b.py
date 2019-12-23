7a27674987f28b0f48553ea474e401afdf8aab5b
unknown <abdulhameedsani125@gmail.com>
2019-12-23 01:59:25 -0800

    added

--- week3/exercise1b.py
+++ week3/exercise1b.py
@@ -1,7 +1,23 @@
 def main():
-	pass
+	my_max(100,10,50,2,3,4,5,6)
+	my_max("musa","hassan")
+	my_max("t","sssssssssssssssssssss")
+	my_max(40,30,0,9,1)
+	my_min(40,30,0,9,1)
+	my_min(100,10,50,2,3,4,5,6)
+	my_min("musa","hassan")
 def my_max(*args):
-	pass
+	i = 0
+	while i < len(args):
+		args = list(args)
+		args.sort()
+		print(args[-1])
+		break		
 def my_min(*args):
-	pass	
+	i = 0
+	while i < len(args):
+		a = list(args)
+		a.sort()
+		print(a[0])
+		break	
 main()
